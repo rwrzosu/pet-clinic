7863382715e2bdf884a273fc5c9eaa502e318fa5
robertw <rwrzosu@gmail.com>
2021-04-18 13:08:00 +0200

    add Unit Tests for SDJPA service. Close #13

--- pet-clinic-data/src/test/java/com/mesh/petclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ pet-clinic-data/src/test/java/com/mesh/petclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -2,72 +2,55 @@ package com.mesh.petclinic.services.springdatajpa;
 
 import com.mesh.petclinic.model.Owner;
 import com.mesh.petclinic.repositories.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerSDJpaServiceTest {
 
+    private static Owner OWNER;
     @Mock
     private OwnerRepository repository;
 
     @InjectMocks
     private OwnerSDJpaService service;
 
-    @Test
-    void findByLastName() {
-        String lastName = "xyz";
-        Owner owner = Owner.builder().firstName("a").lastName(lastName).build();
-        when(repository.findByLastNameContainingIgnoreCase(any())).thenReturn(Set.of(owner));
+    Long ownerId = 1L;
+    String lastName = "Kowalski";
 
-        Owner recivedOwner = service.findByLastName(lastName);
-
-        assertNotNull(recivedOwner);
-        assertEquals(lastName, recivedOwner.getLastName());
-    }
-
-    @Test
-    void findById() {
-        Long id = 111L;
-        Owner owner = Owner.builder().id(id).build();
-        when(repository.findById(any())).thenReturn(Optional.of(owner));
-
-        Owner recivedOwner = service.findById(id);
-
-        verify(repository, times(1)).findById(id);
-        assertEquals(id, recivedOwner.getId());
-    }
-
-    @Test
-    void findAll() {
-        Owner owner1 = Owner.builder().id(1L).build();
-        Owner owner2 = Owner.builder().id(2L).build();
-        when(repository.findAll()).thenReturn(Set.of(owner1, owner2));
-
-        Set<Owner> owners = service.findAll();
-
-        assertEquals(2, owners.size());
+    @BeforeEach
+    void setUp() {
+        OWNER = Owner.builder().id(ownerId).lastName(lastName).build();
     }
 
     @Test
     void save() {
-        var owner = Owner.builder().id(1L).build();
-        when(repository.save(any())).thenReturn(owner);
+        when(repository.save(any())).thenReturn(OWNER);
 
-        Owner saved = service.save(owner);
+        Owner savedOwner = service.save(OWNER);
 
-        assertEquals(1L, saved.getId());
+        assertEquals(OWNER, savedOwner);
+    }
+
+    @Test
+    void findByLastName() {
+        when(repository.findByLastNameContainingIgnoreCase(any())).thenReturn(Set.of(OWNER));
+
+        Owner owner = service.findByLastName(lastName);
+
+        assertNotNull(owner);
+        assertEquals(lastName, owner.getLastName());
     }
 
 }
