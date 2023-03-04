9db3479b00226be7ac4b09db2a6cb5271673c6aa
Robert Wrzos <rwrzosu@gmail.com>
2023-03-04 17:13:53 +0100

    added Visit

---
+++ pet-clinic-data/src/main/java/com/mesh/petclinic/model/Visit.java
@@ -0,0 +1,14 @@
+package com.mesh.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+}
