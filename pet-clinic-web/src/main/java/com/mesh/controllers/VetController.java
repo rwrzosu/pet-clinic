bb22e7085b069519c1244a7987f99bb38c25d493
robertw <rwrzosu@gmail.com>
2024-08-20 12:44:21 +0200

    populated vet list

--- pet-clinic-web/src/main/java/com/mesh/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mesh/controllers/VetController.java
@@ -1,13 +1,26 @@
 package com.mesh.controllers;
 
+import com.mesh.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @GetMapping("/vets")
-    public String index() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping
+    public String vetList(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "/vets/index";
     }
 }
